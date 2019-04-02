6aa45f58e9d65eaeec720812e7cdf748f7822222
winjayyu <winjayyu@gmail.com>
2019-04-02 11:09:53 +0800

    change the warn message

--- src/lib/warn-msg.js
+++ src/lib/warn-msg.js
@@ -1,6 +1,6 @@
 export default {
     name: 'Full name requires a minimum of three characters',
     email: 'The email format is incorrect',
-    confirmEmail: 'The emails entered are not the same. Please re-enter them',
+    confirmEmail: 'The emails entered are not the same',
     serverDefaultError: 'Something went wrong. Please try again'
 }
